c5542b42aecbc3d3ded34613a1779524aa954e24
meteoreinger <reikathr@gmail.com>
2025-04-11 03:49:05 +0700

    finished

--- helper/Actionable.gd
+++ helper/Actionable.gd
@@ -2,19 +2,12 @@ extends Area2D
 
 @export var dialogue_resource : DialogueResource
 @export var dialogue_start: String = "start"
-@export var dialogue_name: String
 @onready var labelPanel = $PanelContainer
 @onready var label = $PanelContainer/Label
 
 func _ready():
 	labelPanel.visible = false
-	write_label(dialogue_name)
-	
-func make_visible():
-	labelPanel.visible = true
-
-func write_label(dialogue_name):
-	label.text = dialogue_name
+	label.text = "Enter"
 	
 func action() -> void:
 	DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start)
